08a70c4f2c8b438ffbc132e0edf22399e5e62bfa
Hossein <hosseinzamani1995@gmail.com>
2022-07-27 12:25:29 +0430

    Echo Generator Function Exam

---
+++ Echo_Generator/echo_gen.m
@@ -0,0 +1,16 @@
+function output = echo_gen(s, Fs, delay, amp)
+    dt = 1/Fs;
+ 
+    N = round(delay/dt);
+    
+    s1 = [s; zeros(N, 1)];
+
+    s2 = [zeros(N, 1); s.*amp];
+
+    output = s1 + s2;
+ 
+    if max(abs(output)) > 1
+        output = output./max(abs(output));
+    end
+
+end
